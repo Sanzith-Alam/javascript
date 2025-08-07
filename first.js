8ba83892c2d1d5c8bab6350326da2bed36005064
Sanzith-Alam <sanzithalam11@gmail.com>
2025-08-07 16:58:58 +0600

    odd even

--- first.js
+++ first.js
@@ -28,9 +28,26 @@
 // }
 
 
-let num = 15;
- if (num%2 == 0){
- console.log(num, "is even") 
- }else{
-  console.log(num, "is odd")
- }
+// let num = 15;
+//  if (num%2 == 0){
+//  console.log(num, "is even") 
+//  }else{
+//   console.log(num, "is odd")
+//  }
+
+
+//ternary operators
+
+// let age = 21;
+//  let result = age >=18 ? "adult" : "not adult";
+//  console.log(result)
+
+//alart("hello");
+// let name = prompt ("hello"); 
+//  console.log(name);
+// let num = prompt ("Enter the number")
+//  if(num % 5 === 0){
+//   console.log(num, "is multiple number 5");
+//  }else{
+//   console.log(num, "is Not a numtiple nmber 5");
+//  }
