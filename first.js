0f0aa32e698430860ef054a21c5041896a185a3d
Sanzith-Alam <sanzithalam11@gmail.com>
2025-08-07 16:13:33 +0600

    condition

--- first.js
+++ first.js
@@ -1,5 +1,28 @@
-let fullName = "Sanzith Alam ";
-let age ="25"
-let x= null;
-let y=undefined;
-console.log(x);
+// const student = {
+//   fullName: "Sanzith",
+//   age: 20, // number, not string
+// };
+
+// student.age = student.age + 1;
+// console.log(student.age); // 21
+
+//Conditional Statement
+
+let age = 25;
+
+// let mode = "dark";
+// let color;
+// if(mode == "dark"){
+//   color = "black";
+// }
+// if (mode === "light"){
+//   color= "white";
+// }
+// console.log(color);
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+
+// if (age < 18) {
+//   console.log("you can not vote");
+// }
